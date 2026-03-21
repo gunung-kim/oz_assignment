6149246757e096f85a3e6f77d7642ee099e6c81f
gunung-kim <unge001@naver.com>
2026-03-21 20:55:11 +0900

    Django 4일차 과제

--- Django/day_2/todo/models.py
+++ Django/day_2/todo/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from datetime import datetime
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Todo(models.Model):
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField()
     start_date = models.DateField(default=datetime.now)
--- Django/day_2/todo/urls.py
+++ Django/day_2/todo/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('',views.todo_list,name='todo_list'),
     path('<int:pk>/',views.todo_detail,name='todo_detail'),
+    path('<int:pk>/update/',views.todo_update,name='todo_update'),
+    path('create/',views.todo_create,name='todo_create'),
+    path('<int:pk>/delete/',views.todo_delete,name='todo_delete'),
 ]
