297ee8b4b8ee14f16d61bfc679faedd9b8aecc87
gunung-kim <unge001@naver.com>
2026-03-23 14:05:01 +0900

    Django 4일차 과제 수정3

--- Django/day_2/todo/forms.py
+++ Django/day_2/todo/forms.py
@@ -7,14 +7,14 @@ class TodoForm(forms.ModelForm):
         model=Todo
         fields=('title','description','start_date','end_date','is_completed')
 
-class TodoCreate(forms.ModelForm):
+class TodoCreate(TodoForm):
 
     class Meta:
         model=Todo
-        fields=('title','description','start_date','end_date')
+        fields=('end_date',)
 
-class TodoUpdate(forms.ModelForm):
-
-    class Meta:
-        model=Todo
-        fields=('title','description','start_date','end_date','is_completed')
+# class TodoUpdate(forms.ModelForm):
+#
+#     class Meta:
+#         model=Todo
+#         fields=('title','description','start_date','end_date','is_completed')
--- Django/day_2/todo/views.py
+++ Django/day_2/todo/views.py
@@ -1,36 +1,37 @@
 from django.shortcuts import render, redirect, get_object_or_404,reverse
 from .models import Todo
-from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from .forms import TodoUpdate,TodoCreate
+from .forms import TodoCreate, TodoForm
 from django.views.decorators.http import require_http_methods
+from django.db.models import Q
 
 def root(request):
     return render(request,'root.html')
 
-@login_required(login_url=settings.LOGIN_URL)
+@login_required()
 def todo_list(request):
-    todos = Todo.objects.all()
-    user_todo = todos.filter(author=request.user).order_by('end_date')
+    user_todo = Todo.objects.filter(author=request.user).order_by('-create_at')
     if not user_todo:
         return render(request,'todo_list.html',{'todos':None,'msg':'아직 게시글이 없습니다'})
     q = request.GET.get('q')
     if q:
-        user_todo = user_todo.filter(title__icontains=q)
+        user_todo = user_todo.filter(
+            Q(title__icontains=q),
+        )
     paginator = Paginator(user_todo,10)
     page = request.GET.get('page',1)
     page_obj = paginator.get_page(page)
     context = {'todos': user_todo,'page_obj':page_obj}
     return render(request,'todo_list.html',context)
 
-@login_required(login_url=settings.LOGIN_URL)
+@login_required()
 def todo_detail(request,pk):
-    target_todo = get_object_or_404(Todo,pk=pk)
-    context = {'target_todo': target_todo}
+    target_todo = get_object_or_404(Todo,pk=pk,author=request.user)
+    context = {'target_todo': target_todo.__dict__}
     return render(request,'todo_detail.html',context)
 
-@login_required(login_url=settings.LOGIN_URL)
+@login_required()
 def todo_create(request):
     form = TodoCreate(request.POST or None)
     if form.is_valid():
@@ -41,22 +42,20 @@ def todo_create(request):
     context = {'form': form}
     return render(request,'todo_create.html',context)
 
-@login_required(login_url=settings.LOGIN_URL)
+@login_required()
 def todo_update(request,pk):
     todo = get_object_or_404(Todo,pk=pk,author=request.user)
     if request.method == 'POST':
-        form = TodoUpdate(request.POST,instance=todo)
+        form = TodoForm(request.POST, instance=todo)
         if form.is_valid():
-            mod_todo = form.save(commit=False)
-            mod_todo.author = request.user
-            mod_todo.save()
-            return redirect(reverse('todo_detail',kwargs={"pk":pk}))
+          form.save()
+        return redirect(reverse('todo_detail',kwargs={"pk":pk}))
     else:
-        form = TodoUpdate(instance=todo)
+        form = TodoForm(instance=todo)
     context = {'form':form}
     return render(request,'todo_update.html',context)
 
-@login_required(login_url=settings.LOGIN_URL)
+@login_required()
 @require_http_methods(['POST'])
 def todo_delete(request,pk):
     todo = get_object_or_404(Todo,pk=pk,author=request.user)
